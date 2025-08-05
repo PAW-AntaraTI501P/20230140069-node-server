29a4f37d3f7e5148f0fcfeb2244ef99b13baadd5
rnktlhr <rinakitluhur10@gmail.com>
2025-08-05 09:44:50 +0700

    update

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3001;
-app.use(express.json());
+const todoRoutes = require("./routes/todo");
+const{todos} = require("./routes/todo");
 
+app.use(express.json());
+app.use("/todos", todoRoutes);
+
+  
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
